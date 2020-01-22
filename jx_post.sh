806d816f0cffa6634ec336349996fa025bc65b14
jianxin <jianxin.liu2017@gmail.com>
2020-01-22 23:25:39 +0800

    post blog and update README.md(commit by script)

--- jx_post.sh
+++ jx_post.sh
@@ -2,7 +2,7 @@
 echo 'add changes'
 git add .
 echo 'commit and add comments'
-git commit -m 'post blog and update README.md'
+git commit -m 'post blog and update README.md(commit by script)'
 echo 'push to master'
 git push origin master
 echo 'Done'
