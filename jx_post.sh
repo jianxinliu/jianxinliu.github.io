483365a8619406ca61e31765e526b2750e69a914
jianxinliu <1159322990@qq.com>
2020-03-25 22:50:51 +0800

    auto post script add comment

--- jx_post.sh
+++ jx_post.sh
@@ -4,7 +4,7 @@ git add .
 
 echo '2. commit and add comments'
 read -p 'enter commit message(no -):'
-git commit -m "$REPLY"
+git commit -m "$REPLY" #  If $REPLY is an empty string, it will give an error. To fix, simply add quotation marks: "$REPLY"
 
 echo '3. push to master'
 git push origin master
