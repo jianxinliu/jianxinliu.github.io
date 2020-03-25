312e5039c4973f8f33d451d4646e0b4554418a28
jianxinliu <1159322990@qq.com>
2020-03-25 22:53:12 +0800

    script

--- jx_post.sh
+++ jx_post.sh
@@ -3,7 +3,7 @@ echo '--1. add all changes'
 git add .
 
 echo '--2. commit and add comments'
-read -p 'enter commit message(no -):'
+read -p 'enter commit message:'
 git commit -m "$REPLY" #  If $REPLY is an empty string, it will give an error. To fix, simply add quotation marks: "$REPLY"
 
 echo '--3. push to master'
