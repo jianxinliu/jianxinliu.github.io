6f4afddcca80328bde8a81e7bbeb8d46f45dc663
jianxinliu <1159322990@qq.com>
2020-03-25 22:51:26 +0800

    test auto post script

--- jx_post.sh
+++ jx_post.sh
@@ -1,12 +1,12 @@
 #/bin/sh
-echo '1. add all changes'
+echo '--1. add all changes'
 git add .
 
-echo '2. commit and add comments'
+echo '--2. commit and add comments'
 read -p 'enter commit message(no -):'
 git commit -m "$REPLY" #  If $REPLY is an empty string, it will give an error. To fix, simply add quotation marks: "$REPLY"
 
-echo '3. push to master'
+echo '--3. push to master'
 git push origin master
 
-echo '4. Done'
+echo '--4. Done'
